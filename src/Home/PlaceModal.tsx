ab2379f01796553d8b03da9f9123aabb2035abc9
Kavitha Palanisamy <archita@kavithas-air.lan>
2022-09-17 11:30:14 -0700

    modal

--- src/Home/PlaceModal.tsx
+++ src/Home/PlaceModal.tsx
@@ -1,14 +1,48 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
+import { Title, Subheading, Text } from "react-native-paper";
+import { A } from "@expo/html-elements";
 
 const PlaceModal: React.FC = ({ route, navigation }) => {
   const [data, setData] = useState<any>(route.params.item);
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 20 }}>{data.name}</Text>
+      <Title style={{ fontSize: 32, marginTop: 50, fontWeight: "bold" }}>
+        {data.name}
+      </Title>
+      <Subheading style={{ fontSize: 14, marginTop: 5 }}>
+        {data.description}
+      </Subheading>
+      <Text style={{ marginHorizontal: 40, marginTop: 25, color: "#595959" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ridiculus mus mauris
+        vitae ultricies leo integer. Id leo in vitae turpis massa.
+      </Text>
+      <Text style={{ fontSize: 14, marginTop: 25 }}>
+        Contact us at <Text style={{ color: "#0096FF" }}>{data.email}</Text>
+      </Text>
+      <Text style={{}}>
+        Our Website:
+        <A style={{ color: "#0096FF" }} href={data.website}>
+          {" "}
+          {data.website}
+        </A>
+      </Text>
+      <Text style={{ fontSize: 14 }}>
+        Call us at <Text style={{ color: "#0096FF" }}>{data.phone}</Text>
+      </Text>
+
       <View style={styles.textContainer}>
-        <Text>Hi</Text>
-        <Button onPress={() => navigation.goBack()} title="Dismiss" />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Subheading
+            style={{ color: "white", fontWeight: "bold", fontSize: 20 }}
+          >
+            OK
+          </Subheading>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -18,10 +52,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
+    backgroundColor: "white",
   },
   textContainer: {
+    // alignItems: "center",
+    marginTop: 50,
+  },
+  button: {
+    backgroundColor: "#0096FF",
+    height: 50,
+    width: 350,
+    borderRadius: 15,
     alignItems: "center",
-    marginTop: "30%",
+    justifyContent: "center",
   },
 });
 
